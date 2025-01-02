f8ac838ec5ed1f72e81a7daf6fc9973524f0a811
DogmaDragon <103123951+DogmaDragon@users.noreply.github.com>
2025-01-02 02:53:48 +0200

    Test

--- build_site.sh
+++ build_site.sh
@@ -75,11 +75,11 @@ buildScraper()
 }
 
 # find all yml files in ./scrapers - these are packages individually
-#for f in ./scrapers/*.yml; do 
-#    buildScraper "$f"
-#done
+for f in ./scrapers/*/*.yml; do 
+    buildScraper "$f"
+done
 
-find ./scrapers/ -mindepth 2 -name *.yml -print0 | while read -d $'\0' f; do
+find ./scrapers/ -mindepth 3 -name *.yml -print0 | while read -d $'\0' f; do
     buildScraper "$f"
 done
 
